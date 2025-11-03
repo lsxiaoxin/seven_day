fe93260784136a6d4df399d0ad3ce3373e53fdf9
Xing\Xiaoxing Xu <2024381054@mails.edu.cn>
2025-11-03 20:20:21 +0800

    geecache day02

---
+++ gee/geecache/geecache.go
@@ -0,0 +1,80 @@
+package geecache
+
+import (
+	"fmt"
+	"sync"
+)
+
+//这样设计 既可以传入实现了Get方法的结构体
+//又可以只传入函数
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+type GetterFunc func(key string) ([]byte, error)
+
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+type Group struct {
+	name string
+	getter Getter
+	mainCache cache
+}
+
+var (
+	mutex sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group{
+	if (getter == nil) {
+		panic("nil getter")
+	}
+	mutex.Lock()
+	defer mutex.Unlock()
+	g := &Group{
+		name: name,
+		getter: getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mutex.RLock()
+	g := groups[name]
+	mutex.RUnlock()
+	return g
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if (key == "") {
+		return ByteView{}, fmt.Errorf("key is empty") 
+	}
+
+	if v, ok := g.mainCache.get(key); ok {
+		return v, nil
+	}
+	return  g.load(key)
+}
+
+func (g *Group) load(key string) (ByteView, error) {
+	return g.getLocally(key);
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b : cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)	
+}

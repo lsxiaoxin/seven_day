cb17bcf96be63c172569882dabc539cc2dad00ae
Xing\Xiaoxing Xu <2024381054@mails.edu.cn>
2025-11-20 09:37:47 +0800

    gee day02

--- gee/gee/main.go
+++ gee/gee/main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"gee/gee"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", indexHandle)
-	http.HandleFunc("/hello", helloHandle)
-	log.Fatal(http.ListenAndServe("localhost:9999", nil))
-}
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
 
-func indexHandle(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "WRL.Path = %q\n", req.URL.Path)
-}
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
 
-func helloHandle(w http.ResponseWriter, req *http.Request) {
-	for k, v := range req.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-	}
+	r.Run(":9999")
 }
